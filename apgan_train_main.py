7c7f2853c1346f81656ce8ccfd7a431f24fd88aa
Ryu0w0 <flymetothemoon0w0@gmail.com>
2020-10-21 13:42:07 +0900

    change arg parameters

--- apgan_train_main.py
+++ apgan_train_main.py
@@ -13,36 +13,32 @@ def initialization():
     from common import logger as log_util
     from common import seed
 
-    # parameters
+    # GENERAL
     parser = argparse.ArgumentParser()
-    parser.add_argument("-load_config_key", type=str, default="default_ch3", help="json config file name")
+    parser.add_argument("-use_gpu", type=int, default=1)
+    parser.add_argument("-is_reproducible", type=int, default=0)
     parser.add_argument("-save_key", type=str, default="apgan",
                         help="Used as a file name of dataset and log files")
+    parser.add_argument("-save_root_path", type=str, default="./files/output/gans")
     parser.add_argument("-num_fixed_noise", type=int, default=16)
     parser.add_argument("-log_level", type=str, default="INFO")
-    # PER X ITERATION
-    parser.add_argument("-logging_per_iter", type=int, default=1)
-    parser.add_argument("-validate_per_iter", type=int, default=5)
-    parser.add_argument("-save_img_per_iter", type=int, default=5)
-    parser.add_argument("-save_model_per_iter", type=int, default=5)
     # MODEL and LOSS
+    parser.add_argument("-load_config_key", type=str, default="default_ch3", help="json config file name")
+    parser.add_argument("-config_root_path", type=str, default="./gans/model/apgan/config/")
     parser.add_argument("-loss_f_type", type=str, default="wgan_gp",
                         help="bce, hinge or wgan_gp")
+    # LOGGING PER X ITERATION
+    parser.add_argument("-logging_per_iter", type=int, default=1)
+    parser.add_argument("-save_img_per_iter", type=int, default=5)
+    parser.add_argument("-save_model_per_iter", type=int, default=5)
     # TRAINING
     parser.add_argument("-step_from", type=int, default=0, help="steps run for range($step_from, $num_steps + 1)")
     parser.add_argument("-num_steps", type=int, default=6, help="used such that train from 0 to $num_steps")
     parser.add_argument("-num_img_iter", type=int, default=1,
                         help="number of iteration per step training for a specific resolution")
-    parser.add_argument("-batch_size_list", type=str, default="2,2,2,2,2,2,2",
+    parser.add_argument("-batch_size_list", type=str, default="256,256,128,64,32,16,8",
                         help="batch_size from 0 to Nth step")  # "256,256,128,64,32,16,8"
     parser.add_argument("-num_workers", type=int, default=1)
-    parser.add_argument("-use_gpu", type=int, default=0)
-    parser.add_argument("-is_reproducible", type=int, default=1)
-    # PATH
-    parser.add_argument("-save_root_path", type=str, default="./files/output/gans")
-    parser.add_argument("-load_root_path", type=str, default="./files/load/gans")
-    parser.add_argument("-load_dataset_root_path", type=str, default="./files/load/prep/dataset/dataset_patch")
-    parser.add_argument("-config_root_path", type=str, default="./gans/model/apgan/config/")
     args = parser.parse_args()
 
     # convert batch_size list into a list
